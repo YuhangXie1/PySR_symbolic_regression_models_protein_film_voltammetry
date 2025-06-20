3b5dd1093e18c7354d337fd6ad8c7bbf71e636a3
Yuhang Xie <yuhangxie1@gmail.com>
2025-06-20 10:51:40 +0100

    single redox ODE updated 2

--- single_redox_ODE copy.py
+++ single_redox_ODE copy.py
@@ -11,58 +11,65 @@ O + ne- --> R
 F = 96485.3321  # s A / mol     #Faradays const
 R = 8.3145      # J/ mol K      #Gas const
 T = 298         # K             #Temperature
-f = F/(R*T)     # s A / J       #Combination of F/RT constants
 
 #Variables
 #n = 1           #               #number of electrons transferred
 A = 1e-4        # m^2           #Electrode area
-Ru = 100         # ohm           #Uncompensated resistance
+Ru = 10         # ohm           #Uncompensated resistance
 Cdl = 1e-3         #               #Double layer capacitance linear coefficient
-gamma = 1e-4       # mol/m^2       #Surface coverage of the protein
+gamma = 1e-6       # mol/m^2       #Surface coverage of the protein
 k0 = 1e-6       # 1/s           #Standard rate constant
 a = 0.3         #               #Alpha = transfer coefficient, 0 <= a <= 1
-E0 = 2          # V             #Potential at equilibrium
+E0 = 0          # V             #Potential at equilibrium
+frequency = 36 #V/s
 
 #kf = k0 * np.exp(-a*f*(E_app-E0)) #Forward reaction rate constant
 #kb = k0 * np.exp((1-a)*f*(E_app-E0)) #Backward reaction rate constant
 
-kf = k0 * (1/3) #Forward reaction rate constant
-kb = k0 - kf #Backward reaction rate constant
+k_red = k0 * (1/3) #Forward reaction rate constant
+k_ox = k0 - k_red #Backward reaction rate constant
 
-#variables
+#input voltage variables
 w = 2*np.pi*36
 p = np.pi/2
 E_amplitude = 0.3
 E_half = 0
 
 #Non dimensionalisation
-kf = kf/k0
-kb = kb/k0
-Ru = ((F**2 * k0)/(R*T))*Ru
-Cdl = ((R*T)/F**2)*Cdl
+#constants
+epsilon = (R*T)/F #V
+tau = epsilon/frequency #s
+iota = (F*A*gamma)/tau #A
+
+#non-dims
+Ru_nd = Ru*(iota/epsilon)
+Cdl_nd = Cdl*(epsilon/(tau*iota))
+k_ox_nd = k_ox * epsilon*tau
+k_red_nd = k_red * epsilon*tau
 
 #equations
 def E_app(t):
     E_app = E_half + E_amplitude * np.sin(w * t + p)
     #Non dimensionalisation
-    E_app = (F/(R*T))*(E_app - E0)
+    E_app = E_app*(1/epsilon)
     return E_app
 
 def dE_app_dt(t):
     dE_app_dt = E_amplitude * w * np.cos(w * t + p)
     #Non dimensionalisation
-    dE_app_dt = (F/(R*T))*dE_app_dt
+    dE_app_dt = dE_app_dt*(tau/epsilon)
     return dE_app_dt
 
 #ODE
 def single_redox_ODE(t, y):
+    #all expressions non-dimensionalised
     O = y[0]
     I_tot = y[1]
 
-    E_eff = E_app(t) - Ru * I_tot
-    dO_dt = E_eff*(kb*(1-O) - kf*O)
+    E_eff = (E_app(t) - Ru_nd * I_tot)
+    dO_dt = E_eff*(k_ox_nd*(1-O) - k_red_nd*O)
 
-    dI_dt = (1/(Ru*Cdl))*(Cdl*dE_app_dt(t) + F*A*gamma* dO_dt - I_tot)
+    dI_dt = (1/Ru_nd)*dE_app_dt(t) + (1/(Ru_nd*Cdl_nd)) * dO_dt - (1/(Ru_nd*Cdl_nd)) * I_tot
 
     return [dO_dt, dI_dt]
 
--- test.py
+++ test.py
@@ -25,15 +25,15 @@ def load_data(Hz):
     Returns numpy arrays of time_data, voltage, current and frequency for a set of data files
     """
     #loading data
-    #data_volt = pd.read_csv(f"Data_for_eq_learning/{Hz}_Hz_2_cv_voltage", sep="\t", names = ["time","voltage"])
-    #data_amp = pd.read_csv(f"Data_for_eq_learning/{Hz}_Hz_2_cv_current", sep="\t", names = ["time","current"])
+    data_volt = pd.read_csv(f"Data_for_eq_learning/{Hz}_Hz_2_cv_voltage", sep="\t", names = ["time","voltage"])
+    data_amp = pd.read_csv(f"Data_for_eq_learning/{Hz}_Hz_2_cv_current", sep="\t", names = ["time","current"])
     
-    data_amp = pd.read_csv(r"Data_for_eq_learning\FTacV_before_PSV_cv_current", sep="\t", names = ["time","current"])
-    data_volt = pd.read_csv(r"Data_for_eq_learning\FTacV_before_PSV_cv_voltage", sep="\t", names = ["time","voltage"])
+    #data_amp = pd.read_csv(r"Data_for_eq_learning\FTacV_before_PSV_cv_current", sep="\t", names = ["time","current"])
+    #data_volt = pd.read_csv(r"Data_for_eq_learning\FTacV_before_PSV_cv_voltage", sep="\t", names = ["time","voltage"])
 
     #slicing data to remove start and end noise. Putting data into tuples
-    slice_start = 0
-    slice_end = -50
+    slice_start = 550
+    slice_end = -100
     time_data = np.array(data_amp["time"].iloc[slice_start:slice_end])
     voltage = np.array(data_volt["voltage"].iloc[slice_start:slice_end])
     current = np.array(data_amp["current"].iloc[slice_start:slice_end])
@@ -57,23 +57,104 @@ def filter_fft(freqs, ft, band_size, Hz, desired_harmonic):
     
     return filtered_ft
 
+def fit_sin(tt, yy):
+    """
+    Fit sin to the input time sequence, and return fitting parameters "amp", "omega", "phase", "offset", "freq", "period" and "fitfunc"
+    Obtained from https://stackoverflow.com/questions/16716302/how-do-i-fit-a-sine-curve-to-my-data-with-pylab-and-numpy, user unsym, Feb 19, 2017
+
+    """
+    tt = np.array(tt)
+    yy = np.array(yy)
+    ff = np.fft.fftfreq(len(tt), (tt[1]-tt[0]))   # assume uniform spacing
+    Fyy = abs(np.fft.fft(yy))
+    guess_freq = abs(ff[np.argmax(Fyy[1:])+1])   # excluding the zero frequency "peak", which is related to offset
+    guess_amp = np.std(yy) * 2.**0.5
+    guess_offset = np.mean(yy)
+    guess = np.array([guess_amp, 2.*np.pi*guess_freq, 0., guess_offset])
+
+    def sinfunc(t, A, w, p, c):  return A * np.sin(w*t + p) + c
+    popt, pcov = curve_fit(sinfunc, tt, yy, p0=guess)
+    A, w, p, c = popt
+    f = w/(2.*np.pi)
+    fitfunc = lambda t: A * np.sin(w*t + p) + c
+    output_dict = {"amp": A, "omega": w, "phase": p, "offset": c, "freq": f, "period": 1./f, "fitfunc": fitfunc, "maxcov": np.max(pcov), "rawres": (guess,popt,pcov)}
+
+    #lambdifying
+    x0 = Symbol("x0")
+    voltage_equation = A*sp.sin(w*x0 + p) + c
+
+    return voltage_equation, [A,w,p,c]
+
+def fit_voltage_eqn(time_data, voltage):
+    voltage_eqn, coeff_array = fit_sin(time_data, voltage)
+    
+    x0 = Symbol("x0")
+    dv_dt_eqn = diff(voltage_eqn, x0)
+
+    return voltage_eqn, dv_dt_eqn, coeff_array
 ####
+files_freq = [36,45,54]
 
-time_data, voltage, current, slice = load_data(0)
+#find voltage eqn and stitch data together
+combined_data_array = np.empty((1,10))
+for Hz in files_freq:
+    
+    #load data
+    time_data, voltage, current, slice_array = load_data(Hz)
+    voltage_eqn, dv_dt_eqn, coeff_array = fit_voltage_eqn(time_data, voltage)
 
+    #current phase and amplitude data
+    phase_data = pd.read_csv(rf"results\20250617-current-phase-original-set\summary.csv")
+    p_val = np.array(phase_data.loc[phase_data["Hz"] == Hz]["phase"])[0]
+    A_val = np.array(phase_data.loc[phase_data["Hz"] == Hz]["amplitude"])[0]
+
+    x0 = Symbol("x0")
+    dv_dt_lambda = lambdify(x0,dv_dt_eqn)
+    dv_dt = dv_dt_lambda(time_data)
+
+    A = np.full((1,len(time_data)), coeff_array[0])[0]
+    w = np.full((1,len(time_data)), coeff_array[1])[0]
+    p = np.full((1,len(time_data)), coeff_array[2])[0]
+    c = np.full((1,len(time_data)), coeff_array[3])[0]
+    p_val = np.full((1,len(time_data)), p_val)[0]
+    A_val = np.full((1,len(time_data)), A_val)[0]
+
+    combined_data_array = np.concatenate([combined_data_array, np.array([time_data, voltage, dv_dt, current, A, w, p, c, A_val, p_val]).T],0)
+
+combined_data_array = np.delete(combined_data_array, (0), axis=0)
+
+#prediction
+time_data = combined_data_array[:,0]
+voltage = combined_data_array[:,1]
+dv_dt = combined_data_array[:,2]
+current = combined_data_array[:,3]
+
+dx = dv_dt
+x = voltage
+A_val = combined_data_array[:,8]
+p_val = combined_data_array[:,9]
+w = combined_data_array[:,5]
+
+current_pred = -0.008968267*A_val*dx - 0.69065799009977*A_val*p_val*x - 0.69065799009977*A_val*x**2 + 5.911939e-6*dx*p_val*x + 2.28552700361524e-6*dx*x + 1.289777e-5*dx + 0.0009932741637787*p_val*x - 0.00023674035*p_val + 5.911939e-6*w*x + 0.0009932741637787*x**2 - 9.4091796e-5
+
+MSE = calculate_MSE(current, current_pred)
 fig, axs = plt.subplots()
-axs.plot(time_data, voltage, label = "voltage")
-axs.set_xlabel("time")
-axs.set_ylabel("voltage")
-axs.set_title("FTacV - voltage-time")
+axs.plot(voltage, current, label = "real", color = "blue")
+axs.plot(voltage, current_pred, label = "pred", color = "red")
+axs.set_xlabel("voltage")
+axs.set_ylabel("current")
+axs.set_title(f"Files:{files_freq} - voltage-current - MSE: {MSE}")
+axs.legend()
 fig.tight_layout()
 plt.show()
 
 fig, axs = plt.subplots()
-axs.plot(time_data, current, label = "voltage")
-axs.set_xlabel("time")
+axs.plot(dv_dt, current, label = "real", color = "blue")
+axs.plot(dv_dt, current_pred, label = "pred", color = "red")
+axs.set_xlabel("dV/dt")
 axs.set_ylabel("current")
-axs.set_title("FTacV - current-time")
+axs.set_title(f"Files:{files_freq} - dv_dt-current - MSE: {MSE}")
+axs.legend()
 fig.tight_layout()
 plt.show()
 
@@ -89,7 +170,7 @@ x, dx, f = symbols("x dx f")
 current_pred_lambda = lambdify([x, dx, f], eqn)
 current_pred = current_pred_lambda(voltage, dv_dt, freq) """
 
-band_size=0.1
+""" band_size=0.1
 freqs, ft = fourier_transform(current, time_data)
 #freqs_pred, ft_pred = fourier_transform(current_pred, time_data)
 
@@ -206,12 +287,6 @@ for harmonic in range(1,10):
 
     log_ft_filtered[~np.isfinite(log_ft_filtered)] = 0
     #log_ft_filtered_pred[~np.isfinite(log_ft_filtered_pred)] = 0
-    """     with open(os.path.join(output_filepath, f"summary.csv"), "a", newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([harmonic,
-                        calculate_MSE_complex(log_ft_filtered,log_ft_filtered_pred),
-                        calculate_MSE_complex(inverseft,inverseft_pred),
-                        ]) """
 
 fig1.legend(handles_1, labels_1, loc='lower right')
 fig1.supxlabel("Frequency (Hz)")
@@ -235,4 +310,4 @@ fig3.supylabel("Current")
 fig3.suptitle(f"File {Hz}Hz harmonics in voltage domain")
 fig3.tight_layout()
 fig3.savefig(os.path.join(output_filepath, f"FTacV-harmonics-voltage-3x3.png"))
-plt.close(fig3.figure)
+plt.close(fig3.figure) """
